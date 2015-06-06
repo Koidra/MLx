e0ff5f78b968edf57f3927b79d242319da8b23b6
Kenneth Tran <one@kentran.net>
2015-06-05 19:26:00 -0700

    pymlx is now pip-able

--- README.md
+++ README.md
@@ -1,13 +1,16 @@
 # MLx
-*A highly scalable machine learning library.*
+*Yet another machine learning framework.*
+
+## How to install
+`pip install pymlx`
 
 ###Requirements
 - A BLAS library installed (e.g. OpenBLAS is free)
 
 
-###Q&As
+##Q&As
 1. Why not using existing open source packages?
-  - We may leverage some existing libraries (e.g. LibLinear or LibSVM)
+  - We may leverage some existing libraries (e.g. [xgboost](https://github.com/dmlc/xgboost) and even scikit-learn)
   - None of the existing libraries satisfies all of our criteria
 
 2. So what are the differentiations of this library
---
+++ setup.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python
+from setuptools import setup
+
+with open('README.md') as file:
+    long_description = file.read()
+
+setup(
+    name='pymlx',
+    packages=['pymlx'],
+    package_data={'pymlx': ['*.so']},
+    version='0.0.7',
+    description='Yet another machine learning framework',
+    long_description=long_description,
+    author='Kenneth Tran',
+    author_email='one@kentran.net',
+    url='https://github.com/zer0n/MLx',
+    keywords="machine learning",
+    license="BSD3",
+    install_requires=[
+        'numpy>=1.9.2',
+        'scipy>=0.14.0',
+        'pandas>=0.15.0',
+        'scikit_learn>=0.16',
+        'matplotlib>=1.4.3',
+        'dill>=0.2.2'],
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: BSD License",
+        "Programming Language :: Python",
+        "Topic :: Database",
+        "Topic :: Database :: Database Engines/Servers",
+        "Operating System :: OS Independent"
+    ]
+)
